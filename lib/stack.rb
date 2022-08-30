f620591ca54c27ffceb6dd59ac47e0db5424c165
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-30 08:41:35 -0700

    commit tree 1ca4b3a19f5725657b8275fbeb976b0910e4c279

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,20 @@
-# your code here
+class Stack
+
+    def initialize
+        @stack = [].limit(4)
+    end
+
+    def push(v)
+        @stack.push(v)
+        self
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+end
